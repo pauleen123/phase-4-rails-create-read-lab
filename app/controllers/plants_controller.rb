6994495820f1bf275814bd40ccb1008360660fbb
+ <+>
2022-11-27 14:18:52 +0300

    commit tree a165923de3bb9e5f1c1d1e24882fcda5ebc61fc8

---
+++ app/controllers/plants_controller.rb
@@ -0,0 +1,25 @@
+class PlantsController < ApplicationController
+
+    #show all plants
+   def index
+    plants = Plant.all 
+    render json: plants
+  end
+
+  def show 
+    plant = Plant.find_by(id: params[:id])
+    render json: plant
+  end
+
+  def create
+    plant = Plant.create(plants_params)
+    render json: plant, status: :created
+end
+
+private
+
+def plants_params 
+    params.permit(:name, :image, :price)
+end
+
+end
